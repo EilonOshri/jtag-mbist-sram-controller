a564add574a89cb6961643153a8cad7c3742a02a
Eilon <eilon337@gmail.com>
2026-09-01 17:21:12 +0300

    Update sram_model.v

--- rtl/sram_model.v
+++ rtl/sram_model.v
@@ -1,21 +1,22 @@
 module SRAM #(
     parameter ADDR_WIDTH = 8,   // 256 Word
-    parameter DATA_WIDTH = 8    //8-bit word width
+    parameter DATA_WIDTH = 8    // 8-bit word width
 )(
     input  wire                  clk,
     input  wire                  ce_n,   // Chip Enable (Active Low)
-  input  wire                  we_n,   // Write Enable ('0' = Write, '1' = Read)
+    input  wire                  we_n,   // Write Enable ('0' = Write, '1' = Read)
     input  wire [ADDR_WIDTH-1:0] addr,   // Address
     input  wire [DATA_WIDTH-1:0] wdata,  // Write Data
     output reg  [DATA_WIDTH-1:0] rdata   // Read Data
 );
-  // 2D register array modeling the SRAM: [DATA_WIDTH-1:0] defines word width, [0:(1<<ADDR_WIDTH)-1] defines depth (2^ADDR_WIDTH words)
-  reg [DATA_WIDTH-1:0] mem [0:(1 << ADDR_WIDTH) - 1];
+
+    // 2D register array modeling the SRAM: [DATA_WIDTH-1:0] defines word width, [0:(1<<ADDR_WIDTH)-1] defines depth (2^ADDR_WIDTH words)
+    reg [DATA_WIDTH-1:0] mem [0:(1 << ADDR_WIDTH) - 1];
   
-  // Synchronous read and write operations
+    // Synchronous read and write operations
     always @(posedge clk) begin
         // Check if the memory block is enabled
-        if (!cs_n) begin
+        if (!ce_n) begin
             if (!we_n) begin
                 // Write Operation: Store wdata into the specified memory address
                 mem[addr] <= wdata;
